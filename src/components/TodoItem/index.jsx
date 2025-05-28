c1ceb16e2a58bc0cc7f6aac5aa57803c9fabcd94
Gabriel Garcia <gabrielgarcia@MBP-de-Gabriel.lan>
2025-05-28 13:39:27 -0500

    edicion de iconos

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,16 +1,16 @@
 import './index.css';
-import { CompleteIcon } from '../../Icons/iconManager';
+import { CompleteIcon } from '../../Icons/CompleteIcon';
 import { DeleteIcon } from '../../Icons/DeleteIcon';
 
 const TodoItem = ( {mensaje, completed, onCompleted, onDelete} ) => {
   return (
     <li className='TodoItem'>
 
-      <CompleteIcon className='iconCheck' onCompleted = {onCompleted} completed={completed}/>
+      <CompleteIcon className='iconCheck' onCompleted = {onCompleted} completed={completed} height="70" width="70"/>
       
       <p className={`taskName ${ completed ? 'taskName-completed' : ''}`}>{mensaje} </p>
         
-      <DeleteIcon className='closeButton' onDelete={onDelete}/>
+      <DeleteIcon className='closeButton' onDelete={onDelete} height={30} width={30}/>
 
     </li>
   );
