7fd3c578da2d52d83500ebf3346378216dc1edfe
Gabriel Garcia <gabrielgarcia@MacBook-Pro-de-Gabriel.local>
2025-06-09 21:13:22 -0500

    ajustando las alturas de MD

--- src/components/Contabilidad/index.jsx
+++ src/components/Contabilidad/index.jsx
@@ -42,58 +42,66 @@ const AccountingModule = () => {
         <div className='contenedorContable'>
             
             <h1 className='table-title'> Contabilidad</h1>
-            
-            <form onSubmit={handleAddCuenta} className='formularioContable'>
-                
-                <select value={nombre} onChange={e => setNombre(e.target.value)} required >
-                    <option value="">Selecciona una cuenta</option>
-                    {
-                        cuentas.map((cuenta, idx) => (
-                            <option key={idx} value={cuenta.nombre}>
-                                {cuenta.nombre}
-                            </option>
-                        ))
-                    }
-                </select>
 
-                <select required style={{ marginLeft: '10px', marginRight: '10px' }} onChange={e => setOpcion(e.target.value)}>
-                    <option value="">Selecciona una opción</option>
-                    <option value="aumenta">Aumenta</option>
-                    <option value="disminuye">Disminuye</option>
-                </select>
-                
-                <input type="number" placeholder="Pasivo" value={opcion == 'aumenta' ? pasivo : activo } onChange={e => {
-                    opcion == 'aumenta' ? setPasivo(e.target.value) : setActivo(e.target.value)
-                
-                }}/>
-                
-                <button type="submit">Agregar</button>
-            </form>
+            <div className='card-view elevation-1'>
 
-            <table >
-                
-                <thead>
-                    <tr>
-                        <th>Cuenta</th>
-                        <th>Activo</th>
-                        <th>Pasivo</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cuentasState.map((acc, index) => (
-                        <tr key={index}>
-                            <td>{acc.nombre}</td>
-                            <td>{acc.activo}</td>
-                            <td>{acc.pasivo}</td>
-                            <td>
-                                <button onClick={() => handleDeleteCuenta(index)}>Eliminar</button>
-                            </td>
+                <form onSubmit={handleAddCuenta} className='formularioContable elevation-2'>
+
+                    <div className='contabilidadInputs'>
+                        <select className='elevation-1' value={nombre} onChange={e => setNombre(e.target.value)} required >
+                            <option value="">Selecciona una cuenta</option>
+                            {
+                                cuentas.map((cuenta, idx) => (
+                                    <option key={idx} value={cuenta.nombre}>
+                                        {cuenta.nombre}
+                                    </option>
+                                ))
+                            }
+                        </select>
+
+                        <select required  className='elevation-1' style={{ marginLeft: '10px', marginRight: '10px' }} onChange={e => setOpcion(e.target.value)}>
+                            <option value="">Selecciona una opción</option>
+                            <option value="aumenta">Aumenta</option>
+                            <option value="disminuye">Disminuye</option>
+                        </select>
+                        
+                        <input className='elevation-1' type="number" placeholder="Pasivo" value={opcion == 'aumenta' ? pasivo : activo } onChange={e => {
+                            opcion == 'aumenta' ? setPasivo(e.target.value) : setActivo(e.target.value)
+                        
+                        }}/>
+
+                    </div>
+                    
+                    <button className='elevation-1' type="submit">Agregar</button>
+                    
+                </form>
+
+                <table >
+                    
+                    <thead>
+                        <tr>
+                            <th>Cuenta</th>
+                            <th>Activo</th>
+                            <th>Pasivo</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            
+                    </thead>
+                    <tbody>
+                        {cuentasState.map((acc, index) => (
+                            <tr key={index}>
+                                <td>{acc.nombre}</td>
+                                <td>{acc.activo}</td>
+                                <td>{acc.pasivo}</td>
+                                <td>
+                                    <button className='elevation-1' onClick={() => handleDeleteCuenta(index)}>Eliminar</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            </div>
+              
         </div>
     );
 };
