54ac236c6c4affc23edbfb08c7f0cb984adcbe23
Gabriel Garcia <gabrielgarcia@MBP-de-Gabriel.lan>
2025-07-12 21:13:41 -0500

    login visualmente completo

--- src/screens/App/index.jsx
+++ src/screens/App/index.jsx
@@ -1,83 +1,19 @@
 import './index.css';
-import { TodoProvider } from '../TodoContext';
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodo } from '../CreateTodo';
-import { LoadingsTodo } from '../LoadingsTodos';
-import { ErrorsTodo } from '../ErrorsTodos';
-import { EmptyTodos } from '../EmptysTodos';
-import { TodoContext } from '../TodoContext';
-import { Modal } from '../../Modal';
+import { Login } from '../Login';
+
+
 import React from 'react';
-import { TodoForm } from '../TodoForm';
-import { NavegadorSuperior } from '../NavegadorSuperior';
-import { AppLogo } from '../AppLogo';
-import {AccountingModule} from '../Contabilidad';
-
- 
-
-/**
- *
- * 
- * 
-const defaultTodos = [
-  { text: ' cebolla', Completed: true },
-  { text: 'tomar el curso de React', Completed: false },
-  { text: 'Llorar con la llorona', Completed: false },
-  { text: 'Cortar cebolla', Completed: false },
-  { text: 'LALALALA ', Completed: false },
-  { text: 'Usar estados derivados ', Completed: true }
-]
-
-localStorage.setItem(itemName, JSON.stringify(defaultTodos));
-
-*/
 
 const App = () =>{
 
-  const { loading, searchedTodo, error, toggleToDo, deleteTodo, openModal } = React.useContext(TodoContext);  
-  
   return(
       <div className='App'>
-        
-        {/* <NavegadorSuperior />
-        <AccountingModule/> */}
-         
-        
-        
-        <div className='contenedorPrincipal elevation-1'>
       
-          <TodoCounter />
-          <TodoSearch />
-        
-          <TodoList>
-            { loading ? <LoadingsTodo/>  : "" }
-            { error ? <ErrorsTodo/>  : "" }
-            { !loading && searchedTodo.length === 0 ? <EmptyTodos/> : "" }
-            { searchedTodo.map( todo => (  
-                <TodoItem  mensaje = { todo.text } key = { todo.text } completed ={todo.Completed}
-                  onCompleted = { ()=>{ toggleToDo(todo.text) }}
-                  onDelete = { () => { deleteTodo(todo.text); }} 
-                /> 
-            ))}
-          </TodoList>
-                  
-          <CreateTodo/>
-
-        </div>
-
-        { openModal ? <Modal> <TodoForm></TodoForm> </Modal>: "" }
-
-      </div> 
+        <Login/>
+      
+      </div>
+         
     )
-
-
-
-
-   
-  
 }
 
 export default App;
--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,94 +1,44 @@
 import React from "react";
 import { TextContentHeading } from "../../components/TextContentHeading";
+// import cookHubIcon from "../../Icons/CookHubImage/image.png";
 import "./index.css";
 
 const Login = () => {
   return (
-    <div className="login" data-model-id="203:1350">
-      <div className="overlap-group-wrapper">
-        <div className="overlap-group">
-          <div className="frame">
-            <div className="ellipse" />
-
-            <img
-              className="logo-icon"
-              alt="Logo icon"
-              src="https://c.animaapp.com/B1uE7PR8/img/logo-icon@2x.png"
-            />
+    <div className="login-container elevation-1">
+      
+      <div className="login-left">
+       
+        <div className="content">
+          <div className="top">
+            <h2>Login</h2>
+            <div>How to i get started lorem ipsum dolor at?</div>
+          </div>
+          
+          <div className="input-container">
+            Username
+            <input type="text" placeholder="Enter your username" />
           </div>
 
-          <div className="div">
-            <TextContentHeading
-              className="text-content-heading-instance"
-              divClassName="design-component-instance-node"
-              heading="Login"
-              size="heading-seccion"
-              subHeading="How to i get started lorem ipsum dolor at?"
-              subheadingClassName="design-component-instance-node"
-            />
-            <div className="input-field">
-              <input className="label" placeholder="Username" type="text" />
-
-              <input className="input" placeholder="Username" type="text" />
-            </div>
-
-            <div className="input-field">
-              <div className="text-wrapper">Password</div>
-
-              <div className="value-wrapper">
-                <div className="value">Password</div>
-              </div>
-            </div>
-
-            <div className="btn-enable">
-              <button className="button">Login Now</button>
-            </div>
-
-            <div className="group">
-              <p className="login-with-others">
-                <span className="span">Login</span>
-
-                <span className="text-wrapper-2"> with Others</span>
-              </p>
-            </div>
-
-            <div className="group-2">
-              <div className="frame-wrapper">
-                <div className="frame-2">
-                  <img
-                    className="img"
-                    alt="Facebook"
-                    src="https://c.animaapp.com/B1uE7PR8/img/facebook-1@2x.png"
-                  />
-
-                  <p className="login-with-facebook">
-                    <span className="text-wrapper-3">Login with </span>
-
-                    <span className="text-wrapper-4">Facebook</span>
-                  </p>
-                </div>
-              </div>
-
-              <div className="div-wrapper">
-                <div className="frame-3">
-                  <img
-                    className="img"
-                    alt="Google"
-                    src="https://c.animaapp.com/B1uE7PR8/img/google-1@2x.png"
-                  />
-
-                  <p className="login-with-google">
-                    <span className="text-wrapper-5">Login with </span>
-
-                    <span className="text-wrapper-4">google</span>
-                  </p>
-                </div>
-              </div>
-            </div>
+          <div className="input-container">
+            Password
+            <input type="password" placeholder="Enter your password" />
           </div>
+          
+          <button className="button elevation-1">Login</button>
+
+
         </div>
       </div>
+      
+      <div className="login-right elevation-1">
+        <div className="webLogo">
+          <img className="image " src="src/Icons/CookHubImage/image.png" alt="" />
+        </div>
+      </div>
+    
+    
     </div>
   );
 };
-export{Login}
+export{ Login };
