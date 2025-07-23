09cf375339f390a932cf763c46c24d57953d2e73
albertold05 <alberto_0550@hotmail.com>
2025-07-22 21:54:17 -0500

    Creacion de el panel admin

---
+++ src/screens/AdminPanel/index.jsx
@@ -0,0 +1,18 @@
+import './index.css';
+import { NavBar } from '../../components/navBarAdmin';
+import logo_nav from '../../assets/image.png';
+
+import React from 'react';
+import { CardList } from '../../components/cardList';
+import { CommentaryBox } from '../../components/commentaryBox';
+
+export const DetailRecipe = () =>{
+
+    return(
+
+        
+        <div></div>
+    )
+}
+
+
