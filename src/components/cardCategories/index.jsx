c0cf081e7a7f27bd5ed2ccab4c77ba36b23c2280
albertold05 <alberto_0550@hotmail.com>
2025-07-29 22:06:27 -0500

    categorias

--- src/components/cardCategories/index.jsx
+++ src/components/cardCategories/index.jsx
@@ -1,14 +1,14 @@
 import './index.css';
 import React from 'react';
 
-export const CardCategories = ({ titulo = "Titulo de Card", image }) => {
+export const CardCategories = ({ titulo = "Titulo de Card", image, onClick, activa = false }) => {
   return (
-    <div className="card-category elevation-1">
-      
+    <div
+      className={`card-category elevation-1 ${activa ? 'activa' : ''}`}
+      onClick={() => onClick(titulo)}
+    >
       <img src={image} alt={titulo} className="image elevation-1" />
-    
       <h4 className="title">{titulo}</h4>
-      
     </div>
   );
 };
