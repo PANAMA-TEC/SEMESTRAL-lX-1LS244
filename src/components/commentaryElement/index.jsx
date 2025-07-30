dd0ae52909b7f6336fbe1b9d8273bf6925ce6481
Gabriel Garcia <gabrielgarcia@MacBook-Pro-de-Gabriel.local>
2025-07-29 22:06:17 -0500

    comentarios operando, pendiente corregir api

--- src/components/commentaryElement/index.jsx
+++ src/components/commentaryElement/index.jsx
@@ -2,29 +2,22 @@ import './index.css';
 import React from 'react';
 import recetas_prototype from '../../assets/recetas_prototype.png';
 
-export const CommentaryElement = ( { data }) => {
-
-  data = {
-    comentario:"Este es un comentario entrando por defecto",
-    nombre: "nombre defecto",
-    imagen: ""
-  }
+export const CommentaryElement = ( { usuario = "", comentario= "" , index = 0 } ) => {
+  
+  
   return (
    <div className="commentaryElement">
       
     <div className="iconocomentario"></div>
      
-
+     
+        <div key= {index} className="comentario elevation-1">
+          <h4> { usuario } </h4>
+          { comentario }
+        </div>
+        
     
-    <div className="comentario elevation-1">
-        <h4> { data.nombre } </h4>
-        { data.comentario }
     </div>
-    
-
-
-    
-  </div>
 
   );
 };
