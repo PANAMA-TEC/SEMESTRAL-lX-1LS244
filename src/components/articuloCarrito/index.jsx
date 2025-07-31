00bbb5fbc9aa7f3689914b317e5e65b1f077bb70
albertold05 <alberto_0550@hotmail.com>
2025-07-31 01:10:12 -0500

    precios

--- src/components/articuloCarrito/index.jsx
+++ src/components/articuloCarrito/index.jsx
@@ -2,16 +2,25 @@ import './index.css';
 import React from 'react';
 
 export const ArticuloCarrito = ({ data }) => {
+  const totalProducto = (data?.price || 0) * (data?.quantity || 1);
+
   return (
     <div className='ArticuloCarrito elevation-1'>
       <div className='left'></div>
       <div className='right'>
-        <h4>{data?.name || "Sin nombre"}</h4>
-        <p>Unidad: {data?.unit || "?"} | Cantidad: {data?.quantity || 0}</p>
+        <h4>{data?.name || "Producto sin nombre"}</h4>
+
+        <p>
+          Unidad: {data?.unit || "-"} <br />
+          Cantidad: {data?.quantity || 1} <br />
+          Precio unitario: ${data?.price?.toFixed(2) || "0.00"} <br />
+          <strong>Total: ${totalProducto.toFixed(2)}</strong>
+        </p>
+
         <div className='options'>
-          <label>-</label> {data?.quantity || 0} <label>+</label>
+          <label>-</label> {data?.quantity || 1} <label>+</label>
         </div>
-      </div>  
+      </div>
     </div>
   );
 };
